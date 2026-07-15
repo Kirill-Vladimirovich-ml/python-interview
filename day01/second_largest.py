1a36595c68be90894a933630820efc6fb4e0c578
Kirill-Vladimirovich-ml <qwryllxd123@mail.ru>
2026-07-15 14:05:17 +0300

    Update second_largest.py

--- day01/second_largest.py
+++ day01/second_largest.py
@@ -1,1 +1,12 @@
+def second_largest(nums):
+    a = nums[0]
+    b = nums[0]
+    for i in range(len(nums)):
+        if a < nums[i]:
+            b = a
+            a = nums[i]
+        if b < nums[i] and b < a and a != nums[i]:
+            b = nums[i]
+
+    return b,a
 
