e68163671dc745d420ed804addfbd6502f8eaa07
Kirill-Vladimirovich-ml <qwryllxd123@mail.ru>
2026-07-15 14:13:16 +0300

    Update count_words.py

--- day01/count_words.py
+++ day01/count_words.py
@@ -1,1 +1,15 @@
+import re
 
+def count_words(text):
+    word_dict = {}
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    words = text.split()
+    
+    for word in words:
+        word_dict[word] = word_dict.get(word, 0) + 1
+        
+    return word_dict
+
+text = "hello world Hello, python world hello"
+print(count_words(text))
