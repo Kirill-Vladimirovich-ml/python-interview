4bcc6dcebc5fdcccdaa6df8f2c25dd40562849b5
Kirill-Vladimirovich-ml <qwryllxd123@mail.ru>
2026-07-15 13:51:22 +0300

    Update flatten.py

--- day01/flatten.py
+++ day01/flatten.py
@@ -1,1 +1,13 @@
+nums = [1, [2, 3], [4, [5, [6, [7]]]]]
 
+def flatten(nums, result=None):
+    if result is None:
+        result = []
+    for i in nums:
+        if isinstance(i, int):
+            result.append(i)
+        else:
+            flatten(i, result)
+    return result
+
+print(flatten(nums))
